7dd089fad969044e58503825a4b8964e69e137ce
Hypermona <mohans8050@gmail.com>
2021-04-02 15:03:05 +0530

    added dark mode and archia font

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
 import React, { useEffect } from "react";
 import "./App.css";
+import Archia from "./fonts/archia-regular-webfont.ttf";
 import { PageView, initGA, Event } from "./Components/Tracking";
 import Main from "./Components/Main";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+const archia = {
+  fontFamily: "Archia",
+  fontStyle: "normal",
+  fontDisplay: "swap",
+  fontWeight: 400,
+  src: `
+    local('Archia'),
+    local('Archia-Regular'),
+    url(${Archia}) format('woff2')
+  `,
+  unicodeRange:
+    "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF",
+};
 
 function App() {
   useEffect(() => {
     initGA("UA-192838043-1");
     PageView();
   }, []);
-  return <Main name="Mohan" />;
+  let preferedMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [darkMode, setDarkMode] = React.useState(false);
+  preferedMode = darkMode;
+  console.log(preferedMode);
+  console.log(darkMode);
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: preferedMode ? "dark" : "light",
+        },
+        typography: {
+          fontFamily: "Archia, Arial",
+        },
+        overrides: {
+          MuiCssBaseline: {
+            "@global": {
+              "@font-face": [archia],
+            },
+          },
+        },
+      }),
+    [preferedMode]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Main name="Mohan" setDarkMode={setDarkMode} />{" "}
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Button from "@material-ui/core/Button";
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -8,9 +8,13 @@ class Main extends React.Component {
     };
   }
   render() {
+    this.props.setDarkMode(false);
     return (
       <div>
         Hello Main {this.state.isDark ? "flase" : "True"} {this.props.name}
+        <Button color="secondary" variant="contained">
+          Hello9
+        </Button>
       </div>
     );
   }
